abce1f30af58da715c1e565fdd746680af3c3608
jameskrw <kangruiwang.cs@gmail.com>
2025-09-28 23:39:34 +0000

    updated workable setup

---
+++ examples/env/setup-pip-viewsuite-tested.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -euo pipefail
+
+
+pip install -U pip
+
+# PyTorch & Deepspeed
+pip install torch==2.8.0 torchaudio==2.8.0 torchvision==0.23.0
+pip install deepspeed==0.17.6 pynvml==13.0.1
+
+# FlashAttention / FlashInfer
+pip install "flash-attn==2.8.3" --no-build-isolation
+pip install flashinfer-python==0.2.11.post3 --no-build-isolation
+
+# SGLang & VLLM
+pip install "sglang[all]==0.5.1"
+pip install vllm==0.10.1
+
+# Megatron & other GPU tools
+pip install megatron-core==0.13.1
+pip install git+https://github.com/garrett4wade/cugae@f0c7198cb3e7265f43218a65ea3db4982520dd08 --no-build-isolation
+
+# Transformers & OpenAI SDK
+pip install transformers==4.56.0
+pip install openai==1.99.6
+
+
+pip install -e .[dev]
+
+
+pip install -e evaluation/latex2sympy
