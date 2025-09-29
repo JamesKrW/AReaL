ba84bbaab8764a463209292b5250e211053d6cdd
jameskrw <kangruiwang.cs@gmail.com>
2025-09-29 03:22:20 +0000

    minor

---
+++ examples/viewsuite/areal_scripts/job_launch/test_areal_gsm8k.sh
@@ -0,0 +1,3 @@
+ python3 -m areal.launcher.local \
+  /mnt/disk/project/viewsuite/AReaL/examples/math/gsm8k_grpo.py \
+  --config /mnt/disk/project/viewsuite/areal_scripts/job_launch/test_areal_gsm8k.yaml
---
+++ examples/viewsuite/areal_scripts/job_launch/test_viewsuite.sh
@@ -0,0 +1,3 @@
+ python3 -m areal.launcher.local \
+  /mnt/disk/project/viewsuite/AReaL/examples/viewsuite/train.py \
+  --config /mnt/disk/project/viewsuite/AReaL/examples/viewsuite/scripts/train_all.yaml
