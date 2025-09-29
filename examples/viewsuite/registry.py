7828506208a89a7a3b522454d9fa469e3be40e68
jameskrw <kangruiwang.cs@gmail.com>
2025-09-29 03:39:57 +0000

    minor

--- examples/viewsuite/registry.py
+++ examples/viewsuite/registry.py
@@ -1,13 +1,13 @@
 from view_suite.envs.view_spatial_bench.gym_view_spatial_no_tool import ViewSpatialNoToolGym
 from view_suite.envs.view_spatial_bench.gym_view_spatial_tool import ViewSpatialToolGym
-from view_suite.envs.sokoban.gym_sokoban import GymSokoban
+# from view_suite.envs.sokoban.gym_sokoban import GymSokoban
 from view_suite.envs.scannet_proxy_task.gym_proxy_tool import GymProxyTool
 from view_suite.envs.scannet_proxy_task.gym_proxy_no_tool import GymProxyNoTool
 
 REGISTERED_ENVS = {
     "ViewSpatialNoToolGym": ViewSpatialNoToolGym,
     "ViewSpatialToolGym": ViewSpatialToolGym,
-    "GymSokoban": GymSokoban,
+    # "GymSokoban": GymSokoban,
     "GymProxyTool": GymProxyTool,
     "GymProxyNoTool": GymProxyNoTool,
 }
