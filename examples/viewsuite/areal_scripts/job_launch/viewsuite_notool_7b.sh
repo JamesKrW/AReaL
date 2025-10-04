72939eab98ff02be4e3725c717ce2213e73ec579
jameskrw <kangruiwang.cs@gmail.com>
2025-10-04 18:36:21 +0000

    updated fileroot

--- examples/viewsuite/areal_scripts/job_launch/viewsuite_notool_7b.sh
+++ examples/viewsuite/areal_scripts/job_launch/viewsuite_notool_7b.sh
@@ -1,3 +1,4 @@
  python3 -m areal.launcher.local \
   AReaL/examples/viewsuite/train.py \
-  --config AReaL/examples/viewsuite/areal_scripts/job_launch/viewsuite_notool_7b.yaml
+  --config AReaL/examples/viewsuite/areal_scripts/job_launch/viewsuite_notool_7b.yaml \
+  > "$(pwd)/$(basename "$0" .sh).log" 2>&1
