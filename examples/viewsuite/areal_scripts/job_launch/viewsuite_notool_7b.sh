2b3bcbac4f4fa04c6b8e9546307f7694c3be006f
jameskrw <kangruiwang.cs@gmail.com>
2025-10-05 21:21:19 +0000

    minor

--- examples/viewsuite/areal_scripts/job_launch/viewsuite_notool_7b.sh
+++ examples/viewsuite/areal_scripts/job_launch/viewsuite_notool_7b.sh
@@ -1,4 +1,4 @@
  python3 -m areal.launcher.local \
-  AReaL/examples/viewsuite/train.py \
+  AReaL/examples/viewsuite/train_xccl_new_eval.py \
   --config AReaL/examples/viewsuite/areal_scripts/job_launch/viewsuite_notool_7b.yaml \
   > "$(pwd)/$(basename "$0" .sh).log" 2>&1
