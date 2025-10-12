588e9615d15f431ee64d7391fba32ba3daacd799
jameskrw <kangruiwang.cs@gmail.com>
2025-10-12 08:19:40 -0500

    updated viewsuite

--- examples/env/setup-pip-viewsuite-tested.sh
+++ examples/env/setup-pip-viewsuite-tested.sh
@@ -5,17 +5,15 @@ set -euo pipefail
 pip install -U pip
 pip uninstall pynvml cugraph-dgl dask-cuda cugraph-service-server raft-dask cugraph cuml cugraph-pyg -y
 # PyTorch & Deepspeed
-pip install deepspeed==0.17.6 pynvml==13.0.1
-pip install torch==2.7.1 torchaudio==2.7.1 torchvision==0.22.1 "deepspeed>=0.17.2" pynvml
+pip install torch==2.8.0 torchaudio torchvision "deepspeed>=0.17.2" pynvml
 # FlashAttention / FlashInfer
 pip install "flash-attn==2.8.3" --no-build-isolation
-pip install flashinfer-python==0.2.11.post3 --no-build-isolation
+pip install flashinfer-python==0.3.1 --no-build-isolation
 
 #pip install vllm==0.10.1
-pip install "sglang[all]==0.4.10.post2"
+pip install "sglang[all]>=0.5.2" 
 # Megatron & other GPU tools
-pip install megatron-core==0.13.1
-pip install git+https://github.com/garrett4wade/cugae@f0c7198cb3e7265f43218a65ea3db4982520dd08 --no-build-isolation
+pip install megatron-core==0.13.1 nvidia-ml-py
 
 
 
