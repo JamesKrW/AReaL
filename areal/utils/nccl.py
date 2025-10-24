48771aafac6ab97cd88523bae24c1b05376e72f3
jameskrw <kangruiwang.cs@gmail.com>
2025-10-24 20:54:47 +0000

    minor

--- areal/utils/nccl.py
+++ areal/utils/nccl.py
@@ -3,4 +3,4 @@ import datetime
 # For large models, generation may consume more than 3600s.
 # We set a large value to avoid NCCL timeout issues during generaiton.
 NCCL_DEFAULT_TIMEOUT = datetime.timedelta(seconds=7200)
-NCCL_NEW_GROUP_DEFAULT_TIMEOUT = datetime.timedelta(seconds=1800)
+NCCL_NEW_GROUP_DEFAULT_TIMEOUT = datetime.timedelta(seconds=7200)
